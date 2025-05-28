1223ed4d22c7f19de699ee0cefc2b2c4abd7cc8e
pba3h23ath <fynn-jannik.thorand@edu.bib.de>
2025-05-28 14:26:30 +0200

    updated nerdfonts

--- flake/desktop/fonts.nix
+++ flake/desktop/fonts.nix
@@ -1,17 +1,18 @@
 {pkgs, ...}: {
-  fonts.packages = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk-sans
-    noto-fonts-emoji
-    liberation_ttf
-    fira-code
-    fira-code-symbols
-    mplus-outline-fonts.githubRelease
-    dina-font
-    proggyfonts
-    nerdfonts
-    helvetica-neue-lt-std
-  ];
+  fonts.packages = with pkgs;
+    [
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-emoji
+      liberation_ttf
+      fira-code
+      fira-code-symbols
+      mplus-outline-fonts.githubRelease
+      dina-font
+      proggyfonts
+      helvetica-neue-lt-std
+    ]
+    ++ builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
 
   fonts.fontDir.enable = true;
 }
